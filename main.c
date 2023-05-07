3632b0fc50b771f1d3cab96fa758968ee53f3d5e
Duckulus <amin.haddou@outlook.de>
2023-05-08 00:09:41 +0200

    try parsing jump

--- main.c
+++ main.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 
-char mem[65536];
-char *rom = mem;
-char *vram = mem + 0x8000;
-char *sram = mem + 0xA000;
-char *wram = mem + 0xC000;
-char *oam = mem + 0xFE00;
-char *io = mem + 0xFF00;
-char *hram = mem + 0xFF80;
-char *ie = mem + 0xFFFF;
+unsigned char mem[65536];
+unsigned char *rom = mem;
+unsigned char *vram = mem + 0x8000;
+unsigned char *sram = mem + 0xA000;
+unsigned char *wram = mem + 0xC000;
+unsigned char *oam = mem + 0xFE00;
+unsigned char *io = mem + 0xFF00;
+unsigned char *hram = mem + 0xFF80;
+unsigned char *ie = mem + 0xFFFF;
 
-char reg[12];
-char *a = reg;
-char *f = reg + 1;
-char *b = reg + 2;
-char *c = reg + 3;
-char *d = reg + 4;
-char *e = reg + 5;
-char *h = reg + 6;
-char *l = reg + 7;
-short *sp = (short*)reg + 8;
-short *pc = (short*)reg + 10;
+unsigned char reg[12];
+unsigned char *a = reg;
+unsigned char *f = reg + 1;
+unsigned char *b = reg + 2;
+unsigned char *c = reg + 3;
+unsigned char *d = reg + 4;
+unsigned char *e = reg + 5;
+unsigned char *h = reg + 6;
+unsigned char *l = reg + 7;
+unsigned short *sp = (unsigned short*)reg + 8;
+unsigned short *pc = (unsigned short*)reg + 10;
 
-void read_file(const char *path, char *dst) {
+void read_file(const char *path, unsigned char *dst) {
   FILE *file = fopen(path, "rb");
 
   if (file == NULL) {
@@ -39,7 +39,17 @@ void read_file(const char *path, char *dst) {
 
 int main(void) {
   read_file("main.gb", mem);
+  *pc=0x100;
+  unsigned char instruction = mem[*pc];
+    printf("Instruction: %hhx\n", instruction);
+  if(instruction==0xc3) {
+      (*pc)++;
+    unsigned char *target = &mem[*pc];
+      *pc = (unsigned short*) *target;
+    printf("Jump to: %hhx %hhx\n", target[0], target[1]);
+  }
 
-  printf("%hhx\n", mem[0x14F]);
+  printf("Instruction pointer: %hhx", *pc);
+
   return 0;
 }
