5db850e98a965f0c66934f3dc56f0aa1501afe14
Eugene Matsumura <eugene.bb1@gmail.com>
2023-05-08 13:18:27 +0200

    add more instructions

--- instruction.c
+++ instruction.c
@@ -1,15 +1,31 @@
 #include "./instruction.h"
+#include "types.h"
 #include "util.h"
 
-void nop(EmulationState *emu) {
+static void nop(EmulationState *emu) {
   // Step to next instruction
-  (*emu->pc)++;
+  *emu->pc += 1;
 }
 
-void jp(EmulationState *emu) {
+static void jp(EmulationState *emu) {
   (*emu->pc)++;
   u16 target = *(u16 *)&emu->mem[*emu->pc];
   *emu->pc = target;
+}
+
+static void ld_b_b(EmulationState *emu) {
+  *emu->b = *emu->b;
+  *emu->pc += 1;
+}
+
+static void ld_d_b(EmulationState *emu) {
+  *emu->d = *emu->b;
+  *emu->pc += 1;
+}
+
+void ld_h_b(EmulationState *emu) {
+  *emu->h = *emu->b;
+  *emu->pc += 1;
 }
 
 Instruction GB_INSTRUCTIONS[GB_INSTRUCTIONS_LENGTH] = {
@@ -19,8 +35,28 @@ Instruction GB_INSTRUCTIONS[GB_INSTRUCTIONS_LENGTH] = {
         .mcycle = 1,
         .execute = &nop,
     },
+    // jp
     {
         .encoding = 0xc3,
         .mcycle = 4,
         .execute = &jp,
-    }};
+    },
+    // ld b, b
+    {
+        .encoding = 0x40,
+        .mcycle = 1,
+        .execute = &ld_b_b,
+    },
+    // ld d, b
+    {
+        .encoding = 0x50,
+        .mcycle = 1,
+        .execute = &ld_d_b,
+    },
+    // ld h, b
+    {
+        .encoding = 0x60,
+        .mcycle = 1,
+        .execute = &ld_h_b,
+    },
+};
--- instruction.h
+++ instruction.h
@@ -9,7 +9,7 @@ typedef struct Instruction {
   void (*execute)(EmulationState *);
 } Instruction;
 
-#define GB_INSTRUCTIONS_LENGTH 2
+#define GB_INSTRUCTIONS_LENGTH 5
 
 extern Instruction GB_INSTRUCTIONS[GB_INSTRUCTIONS_LENGTH];
 
