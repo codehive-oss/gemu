0e45a8f9b97aa46eebfa7118b1995a1e68d4db75
Eugene Matsumura <eugene.bb1@gmail.com>
2023-05-11 18:33:30 +0200

    try to fix tiles

--- ui.c
+++ ui.c
@@ -35,22 +35,20 @@ void win_render_tiles(Window *win, u8 *data) {
   SDL_SetRenderDrawColor(win->renderer, 0xFF, 0xFF, 0xFF, 0xFF);
   SDL_RenderClear(win->renderer);
 
-  for (u8 b = 0; b < 3; b++) {
-    int blockOffset = b * 8 * 8;
+  for (u8 y = 0; y < 24; y++) {
+    int yOffset = y * 8;
+    for (u8 x = 0; x < 16; x++) {
+      int xOffset = x * 8;
 
-    for (u8 y = 0; y < 8; y++) {
-      int yOffset = y * 8;
-      for (u8 x = 0; x < 16; x++) {
-        int xOffset = x * 8;
+      int dataOffset = (x + (y * 16)) * 16;
 
-        for (u8 i = 0; i < 64; i++) {
-          u8 idx = get_palette_idx(data, i);
+      for (u8 i = 0; i < 64; i++) {
+        u8 idx = get_palette_idx(data + dataOffset, i);
 
-          SDL_SetRenderDrawColor(win->renderer, idx * 85, idx * 85, idx * 85,
-                                 255);
+        SDL_SetRenderDrawColor(win->renderer, 0xFF - idx * 85, 0xFF - idx * 85,
+                               0xFF - idx * 85, 0xFF);
 
-          SDL_RenderDrawPoint(win->renderer, xOffset + i % 8, blockOffset + yOffset + i / 8);
-        }
+        SDL_RenderDrawPoint(win->renderer, xOffset + i % 8, yOffset + i / 8);
       }
     }
   }
