e10ad6bb9843f999ff3d8001ca9e0499b32960d4
Eugene Matsumura <eugene.bb1@gmail.com>
2023-05-12 12:17:18 +0200

    fix: use stdint to use the same data size across compilers

--- types.h
+++ types.h
@@ -1,10 +1,11 @@
 #ifndef TYPES_H
 #define TYPES_H
 
+#include <stdint.h>
 #include <stdbool.h>
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
 
 #endif // TYPES_H
