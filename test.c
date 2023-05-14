6a08ec2c75b1c6c1c759c1c9e0f78671bdede317
Eugene Matsumura <eugene.bb1@gmail.com>
2023-05-14 13:39:44 +0200

    feat: add more testing code

--- test.c
+++ test.c
@@ -1,8 +1,16 @@
 #include "./types.h"
 #include "./util.h"
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+void check(const char *description, bool condition) {
+  printf("%s\n", description);
+  assert(condition);
+
+  printf("Passed!\n\n");
+}
 
 void test_palette_idx() {
   u8 b[] = {
@@ -10,10 +18,15 @@ void test_palette_idx() {
       0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF, 0x00, 0xFF,
   };
 
+  bool passed = true;
   for (u8 i = 0; i < 64; i++) {
     u8 idx = get_palette_idx(b, i);
-    printf("%2d: %02X\n", i, idx);
+    if (idx != 0x2) {
+      passed = false;
+    }
   }
+
+  check("Test palette index", passed);
 }
 
 int main(void) { test_palette_idx(); }
